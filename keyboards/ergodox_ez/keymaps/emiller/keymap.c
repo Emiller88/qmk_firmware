1b89d9bc3cc549f10bbe3d385d99fa412b02325e
Edmund Miller <edmund.a.miller@gmail.com>
2018-10-26 20:18:22 -0500

    Update Base layer of Ergodox

--- keyboards/ergodox_ez/keymaps/emiller/keymap.c
+++ keyboards/ergodox_ez/keymaps/emiller/keymap.c
@@ -18,39 +18,39 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  /* Keymap 0: Basic layer
   * ,--------------------------------------------------.           ,--------------------------------------------------.
-  * | NORMAL |   1  |   2  |   3  |   4  |   5  | ⇧⌘P  |           | PROJ |   6  |   7  |   8  |   9  |   0  |   -    |
+  * | `      |   1  |   2  |   3  |   4  |   5  | ⇧⌘P |           | PROJ |   6  |   7  |   8  |   9  |   0  |  BSPC  |
   * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
-  * |    ⇥   |   Q  |   W  |   E  |   R  |   T  |  `   |           |   -  |   Y  |   U  |   I  |   O  |   P  |   \    |
+  * |  Tab   |   Q  |   W  |   E  |   R  |   T  |  `   |           |   -  |   Y  |   U  |   I  |   O  |   P  |   \    |
   * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
-  * |    ⎋   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
+  * |  Esc   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
   * |--------+------+------+------+------+------|   =  |           |   +  |------+------+------+------+------+--------|
   * |   L⇧   |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  |   R⇧   |
   * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
-  *   |  ⌃  |   ⌥  |  (   |  [   |   {  |                                       |  }   |  ]   |   )  |   ⌥   |   ⌃  |
+  *   | CTRL | ALT  |  (   |  [   |   {  |                                       |  }   |  ]   |   )  |   ⌥   | ⌃  |
   *   `----------------------------------'                                       `----------------------------------'
   *                                        ,-------------.       ,-------------.
   *                                        | HOME | END  |       | PGDN | PGUP |
   *                                 ,------|------|------|       |------+------+------.
-  *                                 |SPACE |      |      |       |      |      | BSPC |
-  *                                 |      |NORMAL|------|       |------| ENTER|      |
-  *                                 |   ⌘  |      |      |       |      |      |   ⌘  |
+  *                                 |SPACE |      |      |       |      | BSP  |      |
+  *                                 |      | VIM  |------|       |------|      |Enter |
+  *                                 |  ⌘  |      |      |       |      |  ⌘  |      |
   *                                 `--------------------'       `--------------------'
   */
   [BASE] = LAYOUT_ergodox(
     // Left Hand
-    NOR_MOD,KC_1,   KC_2,   KC_3,    KC_4,   KC_5, KC_ATM,
+    KC_GRAVE,KC_1,  KC_2,   KC_3,    KC_4,   KC_5, KC_TRNS,
     KC_TAB ,KC_Q,   KC_W,   KC_E,    KC_R,   KC_T, KC_GRV,
     KC_ESC ,KC_A,   KC_S,   KC_D,    KC_F,   KC_G,
     KC_LSFT,KC_Z,   KC_X,   KC_C,    KC_V,   KC_B, KC_EQL,
     KC_LCTL,KC_LALT,KC_LPRN,KC_LBRC, KC_LCBR,
 
-                                            KC_HOME,KC_END ,
-                                            TO(SYMB),
-                                            GUI_T(KC_SPC),KC_ESC ,TO_NORM,
+                                           KC_HOME, KC_END,
+                                                    TO(SYMB),
+                             GUI_T(KC_SPC), TO(SYMB), KC_TRNS
 
 
     // Right Hand
-                              KC_ATP , KC_6, KC_7,   KC_8,    KC_9,   KC_0,   KC_MINS,
+                              KC_ATP , KC_6, KC_7,   KC_8,    KC_9,   KC_0,   KC_BSPC,
                               KC_MINS, KC_Y, KC_U,   KC_I,    KC_O,   KC_P,   KC_BSLS,
                                        KC_H, KC_J,   KC_K,    KC_L,   KC_SCLN,KC_QUOT,
                               KC_PLUS, KC_N, KC_M,   KC_COMM, KC_DOT, KC_SLSH,KC_RSFT,
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     KC_PGUP,  KC_PGDN,
     TO(SYMB),
-    MO(MOUSE) , KC_ENT, GUI_T(KC_BSPC)
+    TO(NUM), GUI_T(KC_BSPC),KC_ENT
   ),
 
 /* Keymap 1: Symbol Layer
